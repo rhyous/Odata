d6039a8966781de9c4fd9c950749a00fce71f584
rhyous <rhyous@yahoo.com>
2017-10-05 22:20:20 -0600

    Added Csdl format project

---
+++ src/Rhyous.Odata.Csdl.Tests/CsdlExtensionTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Rhyous.Odata.Csdl.Tests
+{
+    [TestClass]
+    public class CsdlExtensionTests
+    {
+        [TestMethod]
+        public void ToCsdlTests()
+        {
+            // Arrange
+            // Act
+            var csdl = typeof(Person).ToCsdl<Person>();
+
+            // Assert
+            Assert.AreEqual(1, csdl.Keys.Count);
+            Assert.AreEqual("Id", csdl.Keys[0]);
+            Assert.AreEqual(typeof(Person).GetProperties().Length, csdl.Properties.Count);
+
+            Assert.AreEqual("Id", csdl.Properties[0].Name);
+            Assert.AreEqual(1, csdl.Properties[0].CsdlType.Count);
+            Assert.AreEqual("int32", csdl.Properties[0].CsdlFormat);
+
+            Assert.AreEqual("FirstName", csdl.Properties[1].Name);
+            Assert.AreEqual(1, csdl.Properties[1].CsdlType.Count);
+            Assert.AreEqual("", csdl.Properties[1].CsdlFormat);
+
+            Assert.AreEqual("LastName", csdl.Properties[2].Name);
+            Assert.AreEqual(1, csdl.Properties[2].CsdlType.Count);
+            Assert.AreEqual("", csdl.Properties[2].CsdlFormat);
+
+            Assert.AreEqual("DateOfBirth", csdl.Properties[3].Name);
+            Assert.AreEqual(1, csdl.Properties[3].CsdlType.Count);
+            Assert.AreEqual("date", csdl.Properties[3].CsdlFormat);
+        }
+    }
+}
---
+++ src/Rhyous.Odata.Csdl/Extensions/CsdlExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Rhyous.Odata.Csdl
+{
+    public static class CsdlExtensions
+    {
+        public static CsdlEntity<TEntity> ToCsdl<TEntity>(this Type entityType ) {
+            var entity = new CsdlEntity<TEntity> { Keys = new List<string> { "Id" } };
+            foreach (var property in entityType.GetProperties())
+            {
+                var csdlProperty = property.ToCsdl();
+                if (csdlProperty != null)
+                    entity.Properties.Add(csdlProperty);
+            }
+            return entity;
+        }
+
+        private static CsdlProperty ToCsdl(this PropertyInfo property)
+        {
+            var isNullable = property.PropertyType.IsGenericType && property.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>);
+            var propertyType = isNullable ? property.PropertyType.GetGenericArguments()[0] : property.PropertyType;
+            if (propertyType.FullName != null && CsdlTypeDictionary.Instance.ContainsKey(propertyType.FullName))
+            {
+                var csdlTypes = new List<string> { CsdlTypeDictionary.Instance[propertyType.FullName] };
+                if (isNullable)
+                    csdlTypes.Add("null");
+                return new CsdlProperty { Name = property.Name, CsdlType = csdlTypes, CsdlFormat = CsdlFormatDictionary.Instance[propertyType.FullName] };
+            }
+            return null;
+        }
+    }
+}
