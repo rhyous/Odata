14e38d3abbab1a1123ac2836fa1c87d81bb1a031
rhyous <rhyous@yahoo.com>
2017-11-29 23:50:06 -0700

    Added RelatedEntityExclusionAttribute

---
+++ src/Rhyous.Odata/Attributes/RelatedEntityExclusionsAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Rhyous.Odata
+{
+    public class RelatedEntityExclusionsAttribute : Attribute
+    {
+        public RelatedEntityExclusionsAttribute(params string[] exclusions) { Exclusions = exclusions.ToList(); }
+        public List<string> Exclusions { get; set; }
+    }
+}
