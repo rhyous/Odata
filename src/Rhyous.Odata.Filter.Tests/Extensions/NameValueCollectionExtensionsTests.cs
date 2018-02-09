7ab96449b399a14d9aee78103b3ea5151dc98b5e
rhyous <rhyous@yahoo.com>
2018-02-09 14:40:51 -0700

    Moved extension method to Rhyous.Odata.Filter

---
+++ src/Rhyous.Odata.Filter.Tests/Extensions/NameValueCollectionExtensionsTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Rhyous.Odata.Tests.Extensions
+{
+    [TestClass]
+    public class NameValueCollectionExtensionsTests
+    {
+
+        public class TestClass { public int Id { get; set; } }
+
+        [TestMethod]
+        public void GetFilterExpressionNullTest()
+        {
+            // Arrange
+            NameValueCollection collection = null;
+
+            // Act
+            var actual = collection.GetFilterExpression<TestClass>();
+
+            // Assert
+            Assert.IsNull(actual);
+        }
+
+        [TestMethod]
+        public void GetFilterExpressionEmptyTest()
+        {
+            // Arrange
+            NameValueCollection collection = new NameValueCollection();
+
+            // Act
+            var actual = collection.GetFilterExpression<TestClass>();
+
+            // Assert
+            Assert.IsNull(actual);
+        }
+
+        [TestMethod]
+        public void GetFilterExpressionNotEmptyButNoFilterTest()
+        {
+            // Arrange
+            NameValueCollection collection = new NameValueCollection();
+            collection.Add("Key1", "Value1");
+
+            // Act
+            var actual = collection.GetFilterExpression<TestClass>();
+
+            // Assert
+            Assert.IsNull(actual);
+        }
+
+        [TestMethod]
+        public void GetFilterExpressionIdFilterTest()
+        {
+            // Arrange
+            NameValueCollection collection = new NameValueCollection();
+            collection.Add("$filter", "Id eq 1");
+            var t1 = new TestClass { Id = 1 };
+            var t2 = new TestClass { Id = 2 };
+            var list = new List<TestClass> { t1, t2 };
+            
+            // Act
+            var expression = collection.GetFilterExpression<TestClass>();
+            var result = list.AsQueryable().Where(expression).ToList();
+
+            // Assert
+            Assert.AreEqual("e => (e.Id == 1)", expression.ToString());
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(t1, result[0]);
+        }
+    }
+}
---
+++ src/Rhyous.Odata.Filter/Extensions/NameValueCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Rhyous.Collections;
+using System;
+using System.Collections.Specialized;
+using System.Linq.Expressions;
+
+namespace Rhyous.Odata
+{
+    public static class NameValueCollectionExtensions
+    {
+        internal static Expression<Func<T, bool>> GetFilterExpression<T>(this NameValueCollection parameters)
+        {
+            if (parameters == null || parameters.Count == 0)
+                return null;
+            var filterString = parameters.Get("$filter", string.Empty);
+            if (string.IsNullOrWhiteSpace(filterString))
+                return null;
+            return new FilterExpressionBuilder<T>(filterString, new FilterExpressionParser<T>())?.Expression;
+        }
+    }
+}
