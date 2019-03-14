df54bfd5a373da13acb3aaccc96109eafff730fe
Jared A Barneck <rhyous@yahoo.com>
2019-03-14 13:40:34 -0600

    Update NuGet packages to use new License xml tag

---
+++ src/Rhyous.Odata.Csdl/Models/CsdlPath.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Serialization;
+
+namespace Rhyous.Odata.Csdl.Models
+{
+    [DataContract]
+    public class CsdlPath
+    {
+        /// <summary>
+        /// Specifies whether this property is nullable.
+        /// </summary>
+        /// <remarks>See UI.DisplayName example in http://docs.oasis-open.org/odata/odata-csdl-json/v4.01/cs01/odata-csdl-json-v4.01-cs01.html#sec_VocabularyandAnnotation</remarks>
+        [DataMember(Name = "$Path")]
+        public string Path { get; set; }
+    }
+}
