a16a70b60cec1bb8bacf019a7a130ac34eeb4e7a
Jared A Barneck <rhyous@yahoo.com>
2019-03-15 14:23:04 -0600

    Added CsdlPropertyPath

--- src/Rhyous.Odata.Csdl/Models/CsdlPath.cs
+++ src/Rhyous.Odata.Csdl/Models/CsdlPath.cs
@@ -6,9 +6,9 @@ namespace Rhyous.Odata.Csdl
     public class CsdlPath
     {
         /// <summary>
-        /// Specifies whether this property is nullable.
+        /// The path to a value expression.
         /// </summary>
-        /// <remarks>See UI.DisplayName example in http://docs.oasis-open.org/odata/odata-csdl-json/v4.01/cs01/odata-csdl-json-v4.01-cs01.html#sec_VocabularyandAnnotation</remarks>
+        /// <remarks>See example in http://docs.oasis-open.org/odata/odata-csdl-json/v4.01/cs01/odata-csdl-json-v4.01-cs01.html#sec_ValuePath</remarks>
         [DataMember(Name = "$Path")]
         public string Path { get; set; }
     }
