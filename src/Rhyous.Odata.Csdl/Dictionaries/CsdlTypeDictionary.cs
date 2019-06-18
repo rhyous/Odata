aa5aa6058134bb4b1dbe7fa8152472ebaa69351d
Jared A Barneck <rhyous@yahoo.com>
2019-06-18 15:01:36 -0600

    Add Dictionary values bidirectionally

--- src/Rhyous.Odata.Csdl/Dictionaries/CsdlTypeDictionary.cs
+++ src/Rhyous.Odata.Csdl/Dictionaries/CsdlTypeDictionary.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Concurrent;
+﻿using System.Collections.Concurrent;
 
 namespace Rhyous.Odata.Csdl
 {
@@ -17,40 +16,53 @@ namespace Rhyous.Odata.Csdl
 
         public void Init()
         {
-            GetOrAdd("System.Binary", "Edm.Binary");
-            GetOrAdd("System.Boolean", "Edm.Boolean");
-            GetOrAdd("System.Byte", "Edm.Byte");
-            GetOrAdd("System.DateTime", "Edm.Date");
-            GetOrAdd("System.DateTimeOffset", "Edm.DateTimeOffset");
-            GetOrAdd("System.Decimal", "Edm.Decimal");
-            GetOrAdd("System.Double", "Edm.Double");
-            GetOrAdd("System.Duration", "Edm.Duration");
-            GetOrAdd("System.Enum", "Edm.Enum");
-            GetOrAdd("System.Guid", "Edm.Guid");
-            GetOrAdd("System.Int16", "Edm.Int16");
-            GetOrAdd("System.Int32", "Edm.Int32");
-            GetOrAdd("System.Int64", "Edm.Int64");
-            GetOrAdd("System.SByte", "Edm.SByte");
-            GetOrAdd("System.Single", "Edm.Single");
-            GetOrAdd("System.Stream", "Edm.Stream");
-            GetOrAdd("System.String", "Edm.String");
-            GetOrAdd("System.TimeOfDay", "Edm.TimeOfDay");
-            GetOrAdd("System.Geography", "Edm.Geography");
-            GetOrAdd("System.GeographyPoint", "Edm.GeographyPoint");
-            GetOrAdd("System.GeographyLineString", "Edm.GeographyLineString");
-            GetOrAdd("System.GeographyPolygon", "Edm.GeographyPolygon");
-            GetOrAdd("System.GeographyMultiPoint", "Edm.GeographyMultiPoint");
-            GetOrAdd("System.GeographyMultiLineString", "Edm.GeographyMultiLineString");
-            GetOrAdd("System.GeographyMultiPolygon", "Edm.GeographyMultiPolygon");
-            GetOrAdd("System.GeographyCollection", "Edm.GeographyCollection");
-            GetOrAdd("System.Geometry", "Edm.Geometry");
-            GetOrAdd("System.GeometryPoint", "Edm.GeometryPoint");
-            GetOrAdd("System.GeometryLineString", "Edm.GeometryLineString");
-            GetOrAdd("System.GeometryPolygon", "Edm.GeometryPolygon");
-            GetOrAdd("System.GeometryMultiPoint", "Edm.GeometryMultiPoint");
-            GetOrAdd("System.GeometryMultiLineString", "Edm.GeometryMultiLineString");
-            GetOrAdd("System.GeometryMultiPolygon", "Edm.GeometryMultiPolygon");
-            GetOrAdd("System.GeometryCollection", "Edm.GeometryCollection");
+            AddBidirectionally("System.Binary", "Edm.Binary");
+            AddBidirectionally("System.Boolean", "Edm.Boolean");
+            AddBidirectionally("System.Byte", "Edm.Byte");
+            AddBidirectionally("System.DateTime", "Edm.Date");
+            AddBidirectionally("System.DateTimeOffset", "Edm.DateTimeOffset");
+            AddBidirectionally("System.Decimal", "Edm.Decimal");
+            AddBidirectionally("System.Double", "Edm.Double");
+            AddBidirectionally("System.Duration", "Edm.Duration");
+            AddBidirectionally("System.Enum", "Edm.Enum");
+            AddBidirectionally("System.Guid", "Edm.Guid");
+            AddBidirectionally("System.Int16", "Edm.Int16");
+            AddBidirectionally("System.Int32", "Edm.Int32");
+            AddBidirectionally("System.Int64", "Edm.Int64");
+            AddBidirectionally("System.SByte", "Edm.SByte");
+            AddBidirectionally("System.Single", "Edm.Single");
+            AddBidirectionally("System.Stream", "Edm.Stream");
+            AddBidirectionally("System.String", "Edm.String");
+            AddBidirectionally("System.TimeOfDay", "Edm.TimeOfDay");
+            AddBidirectionally("System.Geography", "Edm.Geography");
+            AddBidirectionally("System.GeographyPoint", "Edm.GeographyPoint");
+            AddBidirectionally("System.GeographyLineString", "Edm.GeographyLineString");
+            AddBidirectionally("System.GeographyPolygon", "Edm.GeographyPolygon");
+            AddBidirectionally("System.GeographyMultiPoint", "Edm.GeographyMultiPoint");
+            AddBidirectionally("System.GeographyMultiLineString", "Edm.GeographyMultiLineString");
+            AddBidirectionally("System.GeographyMultiPolygon", "Edm.GeographyMultiPolygon");
+            AddBidirectionally("System.GeographyCollection", "Edm.GeographyCollection");
+            AddBidirectionally("System.Geometry", "Edm.Geometry");
+            AddBidirectionally("System.GeometryPoint", "Edm.GeometryPoint");
+            AddBidirectionally("System.GeometryLineString", "Edm.GeometryLineString");
+            AddBidirectionally("System.GeometryPolygon", "Edm.GeometryPolygon");
+            AddBidirectionally("System.GeometryMultiPoint", "Edm.GeometryMultiPoint");
+            AddBidirectionally("System.GeometryMultiLineString", "Edm.GeometryMultiLineString");
+            AddBidirectionally("System.GeometryMultiPolygon", "Edm.GeometryMultiPolygon");
+            AddBidirectionally("System.GeometryCollection", "Edm.GeometryCollection");
+        }
+
+        /// <summary>
+        /// This method make the dictionary bidirectional. 
+        /// 1. The string a is a Key with a value of string b.
+        /// 1. The string b is a Key with a value of string a.
+        /// </summary>
+        /// <param name="a">The first string.</param>
+        /// <param name="b">The second string.</param>
+        public void AddBidirectionally(string a, string b)
+        {
+            GetOrAdd(a, b);
+            GetOrAdd(b, a);
         }
     }
 }
