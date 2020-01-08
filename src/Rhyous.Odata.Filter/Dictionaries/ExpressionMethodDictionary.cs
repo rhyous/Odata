7e68bd739d6554a3868249bc826ad8da96bc594c
Jared A Barneck <rhyous@yahoo.com>
2020-01-08 16:22:48 -0700

    Ability to use actual operators (=, !=, >, >=, <, <=) instead of just the names

--- src/Rhyous.Odata.Filter/Dictionaries/ExpressionMethodDictionary.cs
+++ src/Rhyous.Odata.Filter/Dictionaries/ExpressionMethodDictionary.cs
@@ -14,12 +14,18 @@ namespace Rhyous.Odata
 
         internal ExpressionMethodDictionary() : base(StringComparer.OrdinalIgnoreCase)
         {
+            GetOrAdd("=", (a, b) => Expression.Equal(a, b));
             GetOrAdd("eq", (a, b) => Expression.Equal(a, b));
             GetOrAdd("ne", (a, b) => Expression.NotEqual(a, b));
+            GetOrAdd("!=", (a, b) => Expression.NotEqual(a, b));
             GetOrAdd("gt", (a, b) => Expression.GreaterThan(a, b));
+            GetOrAdd(">", (a, b) => Expression.GreaterThan(a, b));
             GetOrAdd("ge", (a, b) => Expression.GreaterThanOrEqual(a, b));
+            GetOrAdd(">=", (a, b) => Expression.GreaterThanOrEqual(a, b));
             GetOrAdd("lt", (a, b) => Expression.LessThan(a, b));
+            GetOrAdd("<", (a, b) => Expression.LessThan(a, b));
             GetOrAdd("le", (a, b) => Expression.LessThanOrEqual(a, b));
+            GetOrAdd("<=", (a, b) => Expression.LessThanOrEqual(a, b));
             GetOrAdd("and", (a, b) => Expression.AndAlso(a, b));
             GetOrAdd("or", (a, b) => Expression.OrElse(a, b));
         }
