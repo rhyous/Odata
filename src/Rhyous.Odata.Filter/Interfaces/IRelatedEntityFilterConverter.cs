1523af0c8d46e6b9f76e01d8233b2c7531244a61
Jared Barneck <rhyous@yahoo.com>
2022-03-04 15:49:58 -0700

    Fixed missing comment

--- src/Rhyous.Odata.Filter/Interfaces/IRelatedEntityFilterConverter.cs
+++ src/Rhyous.Odata.Filter/Interfaces/IRelatedEntityFilterConverter.cs
@@ -1,7 +1,7 @@
-﻿using System.Threading.Tasks;
-
-namespace Rhyous.Odata.Filter
+﻿namespace Rhyous.Odata.Filter
 {
+    /// <summary>The interface for a RelatedEntityFilterConverter.</summary>
+    /// <typeparam name="TEntity">The type of Entity this converter is for.</typeparam>
     public interface IRelatedEntityFilterConverter<TEntity> : IFilterConverter<TEntity>
     {
     }
